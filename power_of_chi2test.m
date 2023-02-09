d8ff2822ad28e7a8ac7c25054ee472a1dd462f0f
zzx0220 <54050928+zzx0220@users.noreply.github.com>
2023-02-10 00:45:46 +0800

    power analysis of two tests

--- power_of_chi2test.m
+++ power_of_chi2test.m
@@ -13,12 +13,12 @@ for rp = 1:1000
     
             %% chi2 test
             % calculate theta0 of each subject
-            [pxx,f] = pwelch(y_sin,ones(1,60),[],[],120);
+            [pxx,f] = pwelch(y_sin,ones(1,60),[],60,120);
             pxx = pxx(2:(end-1),:);
             f = f(2:(end-1));
             
             pxx = pxx./mean(pxx,1)*2;
-            lambda = sum(pxx(26,:));
+            lambda = sum(pxx(6,:));
             p = 1-chi2cdf(lambda,2*n);
             if p<0.05
                 H_chi2(rp,amp,n) = 1;
@@ -43,26 +43,4 @@ g.set_names('x','amplitude','y','power','lightness','sample number');
 g.set_color_options("legend","merge");
 g.geom_hline('yintercept',0.8,'style','k--');
 g.draw();
-%g.export('file_name','power_chi2test','file_type','png','width',15,'height',10);
-
-% noise = randn(60,1);
-% amp = 0.3;
-% T = 20;
-% x = [1:60]';
-% tau = rand*2*pi;
-% y_sin = sin((x)/T*2*pi+tau).*amp;
-% y = y_sin+noise;
-% 
-% figure;plot(y)
-% hold on;plot(noise)
-% hold on;plot(y_sin)
-% [pxx,f]=pwelch(y,ones(1,60),[],60,120);
-% figure;plot(f,pxx)
-% 
-% acf = resp_sm;
-% back_delay_frames = [36:95];
-% Fs = 120; %Hz
-% T = 1/Fs; %period
-% L = size(acf,1);
-% hanningWin = hanning(L,'periodic'); % use periodic method as suggested by MATLAB for further fft analysis
-% n =256;
+g.export('file_name','power_chi2test','file_type','png','width',15,'height',10);
--- power_of_shapetest.m
+++ power_of_shapetest.m
@@ -1,39 +1,73 @@
+for i = 1:1000
+    freq = normrnd(12,1);
+    T = 60/freq*2;
+    x = [1:60]';
+    tau = rand*2*pi;
+    y_sin = sin((x)/T*2*pi+tau);
+    [pxx(:,i),f]=pwelch(y_sin,ones(1,60),[],60,120);
+end
 
-n = 1;H_chi2=[];H_perm=[];H_perm2=[];
+pxx_prop = pxx(2:(end-1),:)./sum(pxx(2:(end-1),:),1);
+prop_r = mean(pxx_prop,2); % set as the alternative hypothesis
+
+% 
+% figure;clear g
+% g = gramm('x',2:2:60,'y',[mean(pxx_prop,2)'./2;normpdf(2:2:60,12,1)],'color',{'sim','true'});
+% g.geom_line();
+% g.set_names('x','frequency','y','power','color','data');
+% g.draw();
+% g.export('file_name','psd_est_norm','file_type','png','width',10,'height',7.5);
+
+
+%% generate distribution under null hypothesis
+prop_null = 1/29;
+
+% prop_r = zeros(30,1)*;
+% prop_r(6) = 1;
+
+for n = 1:20
+    pxx_rp=[];
+for i = 1:1000
+    noise = randn(60,n);
+    [pxx_rp(:,:,i),f]=pwelch(noise,ones(1,60),[],60,120);
+end
+f = f(2:(end-1));
+band = (f>=10 & f<=14);
+
+pxx_prop_rp = pxx_rp(2:(end-1),:,:)./sum(pxx_rp(2:(end-1),:,:),1);
+
+lambda_rp = squeeze(sum(sum(pxx_prop_rp(band,:,:)./prop_r(band),1),2) - sum(sum(pxx_prop_rp(band,:,:)./prop_null,1),2));
+
+%figure;histogram(lambda_rp);
+
+%% simulate data
 amplevel = [0:0.05:0.5];
-for rp = 1:1000
-    for amp = 1:length(amplevel)
-        for n = 1:20
-            noise = randn(60,n);
-            
-            T = 10;
-            x = [1:60]';
-            tau = rand(1,n)*2*pi;
-            y_sin = sin((x)/T*2*pi+tau).*amplevel(amp)+noise;
+for amp = 1:length(amplevel)
+    for rp = 1:1000
+        noise = randn(60,n);
+        freq = 12;%normrnd(12,1);
+        T = 60/freq*2;
+        x = [1:60]';
+        tau = rand*2*pi;
+        y_sin = sin((x)/T*2*pi+tau).*amplevel(amp)+noise;
+        [pxx,f]=pwelch(y_sin,ones(1,60),[],60,120);
     
-            %% chi2 test
-            % calculate theta0 of each subject
-            [pxx,f] = pwelch(y_sin,ones(1,60),[],60,120);
-            pxx = pxx(2:31,:);
-            f = f(2:31);
-            
-            pxx = pxx./mean(pxx,1)*2;
-            lambda = sum(pxx(6,:));
-            p = 1-chi2cdf(lambda,2*n);
-            if p<0.05
-                H_chi2(rp,amp,n) = 1;
-            else
-                H_chi2(rp,amp,n) = 0;
-            end
+        pxx_prop = pxx(2:(end-1),:)./sum(pxx(2:(end-1),:),1);
+        lambda = squeeze(sum(sum(pxx_prop(band,:)./prop_r(band),1),2) - sum(sum(pxx_prop(band,:)./prop_null,1),2));
+        
+        p = sum(lambda>lambda_rp)./1000;
+        if p<0.05
+            H_chi2(rp,amp,n) = 1;
+        else
+            H_chi2(rp,amp,n) = 0;
         end
     end
 end
+end
 
-squeeze(mean(H_chi2));
 H = permute(H_chi2,[2 1 3]);
 H = reshape(H,[size(H,1) size(H,2)*size(H,3)]);
 H = H';
-H_t = H+rand*0.01;
 subjnum = repelem(1:n,1,1000);
 
 figure;clear g
@@ -43,26 +77,4 @@ g.set_names('x','amplitude','y','power','lightness','sample number');
 g.set_color_options("legend","merge");
 g.geom_hline('yintercept',0.8,'style','k--');
 g.draw();
-g.export('file_name','power_chi2test','file_type','png','width',15,'height',10);
-
-noise = randn(60,1);
-amp = 0.3;
-T = 20;
-x = [1:60]';
-tau = rand*2*pi;
-y_sin = sin((x)/T*2*pi+tau).*amp;
-y = y_sin+noise;
-
-figure;plot(y)
-hold on;plot(noise)
-hold on;plot(y_sin)
-[pxx,f]=pwelch(y,ones(1,60),[],60,120);
-figure;plot(f,pxx)
-
-acf = resp_sm;
-back_delay_frames = [36:95];
-Fs = 120; %Hz
-T = 1/Fs; %period
-L = size(acf,1);
-hanningWin = hanning(L,'periodic'); % use periodic method as suggested by MATLAB for further fft analysis
-n =256;
+g.export('file_name','power_shapetest','file_type','png','width',15,'height',10);
